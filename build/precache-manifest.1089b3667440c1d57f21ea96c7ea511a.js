c95818cef7e297ff267a785607d1f6ae6c2f3e12
KwyjibojonesJr <KwyjibojonesJr@gmail.com>
2019-03-12 13:41:33 -0400

    Post .gitignore changes

---
+++ build/precache-manifest.1089b3667440c1d57f21ea96c7ea511a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2d6bde677aeff7f5fc88",
+    "url": "/static/js/main.2d6bde67.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "2d6bde677aeff7f5fc88",
+    "url": "/static/css/main.e8fe7005.chunk.css"
+  },
+  {
+    "revision": "e27dea9e6bc1e0d4d832d04a37f68222",
+    "url": "/index.html"
+  }
+];
